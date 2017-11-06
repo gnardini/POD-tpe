349411e108d7a31d7c5f906dfc95a787ebc116df
gnardini <gonzalo.nardini.dev@gmail.com>
2017-11-06 16:48:18 -0300

    Fix run client script

--- run-client.sh
+++ run-client.sh
@@ -1,2 +1,2 @@
-mvn clean install
-java -Daddresses=10.17.68.19 -Dquery=1 -DinPath=census100.csv -DoutPath=out.txt -DtimeOutPath=time.txt -Dn=10 -Dprov="Santa Fe" -cp target/grupo3-1.0-SNAPSHOT.jar Client
+mvn clean compile assembly:single
+java -Daddresses=10.17.68.19 -Dquery=1 -DinPath=census100.csv -DoutPath=out.txt -DtimeOutPath=time.txt -Dn=10 -Dprov="Santa Fe" -cp target/grupo3-1.0-SNAPSHOT-jar-with-dependencies.jar Client
